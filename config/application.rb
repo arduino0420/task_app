b11fc138dc6642d5c712f9b9be14ecc7aff7cea5
Yuki <arduino0420@gmail.com>
2026-09-14 17:12:51 +0900

    use imagemagick for active storage

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module TaskApp
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    config.active_storage.variant_processor = :mini_magick
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -25,3 +27,4 @@ module TaskApp
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
+
